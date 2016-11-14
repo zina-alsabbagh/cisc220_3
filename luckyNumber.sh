eb2fdf50e0afe8e9d073589405de6a987e29c7ba
Zina Al-Sabbagh <14zas@queensu.ca>
2016-11-14 18:52:38 -0500

    Final luckyNumber.sh

--- luckyNumber.sh
+++ luckyNumber.sh
@@ -4,10 +4,11 @@
 #!/bin/bash
 
 luckyNumber() {
+
 	numOne=$1
 	total=0
 
-	for i in $(seq = $((${#numOne}-1))) ; do
+	for i in $(seq 0 $((${#numOne}-1))) ; do
 		num=${numOne:$i:1}
 		total=$(($total+$num))
 	done
@@ -22,4 +23,13 @@ for ((j=1000;j<10000;j++)) ; do
 	luckyNum=$(luckyNumber $tracker);
 	if [[ $luckyNum -eq 7 ]] ; then
 		echo $tracker;
+	elif [[ ${#luckyNum} -eq 1 ]] ; then
+		if [[ $luckyNum -eq 7 ]] ; then
+			echo $tracker;
+		fi;
+	elif [[ ${#luckyNum} -gt 1 ]] ; then
+		if [[ $(luckyNumber $tracker) -eq 7 ]] ; then
+			echo $tracker;
+		fi;
+	fi;
 done
