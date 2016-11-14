05683f8e645abf1fabe35ac90e3e47a2a8929503
Zina Al-Sabbagh <14zas@queensu.ca>
2016-11-14 18:57:10 -0500

    commit 2

--- tpircs.sh
+++ tpircs.sh
@@ -1,1 +1,14 @@
+# Rita Alsattah 10154610
+# Zina Al-Sabbagh 10161380
 
+#!/bin/bash
+
+read -p "Insert a list of words: " -a words
+
+lenArr=${#words[*])
+ctr=0
+
+for ((n=0;n<lenArr;n++)) ; do
+done
+
+echo "Output: ${words[*]_}"
