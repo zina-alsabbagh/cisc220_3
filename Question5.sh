1231518b8e395bdd9a30db007873129769e14fd2
Zina Al-Sabbagh <14zas@queensu.ca>
2016-11-14 20:03:58 -0500

    Final Question5.sh

--- Question5.sh
+++ Question5.sh
@@ -1,1 +1,10 @@
+# Rita Alsattah 10154610
+# Zina Al-Sabbagh
 
+#!/bin/bash
+
+lastString="#Developed in CISC220"
+bin="#!/bin"
+bash="/bash"
+
+find . -type f -executable | sed -i "s,$bin$bash,$bin$bash\n$lastString," *.sh
