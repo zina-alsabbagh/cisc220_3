d1f3f21f18c89f6a80b18534cb4220c16c243133
Zina Al-Sabbagh <14zas@queensu.ca>
2016-11-14 19:20:13 -0500

    Final passwordChecker.sh

--- passwordChecker.sh
+++ passwordChecker.sh
@@ -3,6 +3,16 @@
 
 #!/bin/bash
 
+printf "\n"
+echo "[1] Minimum length of 8 characters."
+printf "\n"
+echo "[2] Must contain at least one numeric character."
+printf "\n"
+echo "[3] Must contain at least one of the following non-alphabetic characters: @, #, $, %, &, *, +, -, =."
+printf "\n"
+echo "Password is "Weak" when one condition is satisfied, "Moderate" when two conditions are satisfied, and "Strong" if all are satisfied."
+printf "\n"
+
 read -p "Please enter a password: " -s pass
 
 condition=0
