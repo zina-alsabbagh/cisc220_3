f842266d37e111c26b8fd3d01ff0a5ebd9a943a7
Zina Al-Sabbagh <14zas@queensu.ca>
2016-11-14 19:03:36 -0500

    Final tpircs.sh

--- tpircs.sh
+++ tpircs.sh
@@ -5,10 +5,18 @@
 
 read -p "Insert a list of words: " -a words
 
-lenArr=${#words[*])
+lenArr=${#words[*]}
 ctr=0
 
-for ((n=0;n<lenArr;n++)) ; do
+for ((n=0; n<lenArr; n++)) ; do
+	backword=""
+	word=${words[n]}
+	for ((i=${#word}-1; i>=0; i--)) ; do
+		backword="$backword${word:$i:1}";
+	done
+	words[ctr]=$backword
+	((ctr++))
+
 done
 
-echo "Output: ${words[*]_}"
+echo "Output: ${words[*]}"
