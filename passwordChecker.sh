5ae87c5ab33a735345c3f792aadcb3727de7b4c6
Zina Al-Sabbagh <14zas@queensu.ca>
2016-11-14 19:10:07 -0500

    added conditions

--- passwordChecker.sh
+++ passwordChecker.sh
@@ -1,1 +1,20 @@
+# Rita Alsattah 10154610
+# Zina Al-Sabbagh 10161380
 
+#!/bin/bash
+
+read -p "Please enter a password: " -s pass
+
+condition=0
+
+if (( ${#pass}>=8 )) ; then
+	((condition++))
+fi
+
+if [[ $pass =~ [0-9] ]] ; then
+	((condition++))
+fi
+
+if [[ $pass =~ [@#$%*+-=\&] ]] ; then
+	((condition++))
+fi
